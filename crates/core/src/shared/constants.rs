10c0b8bf9a4a9934f9a0e3fb0e8ef3992838f3c7
Joel <da1nerd@pm.me>
2026-02-27 23:42:23 +0700

    changed model download links

--- crates/core/src/shared/constants.rs
+++ crates/core/src/shared/constants.rs
@@ -1,10 +1,10 @@
 pub const YOLO_MODEL_NAME: &str = "yolo11n-pose_widerface.onnx";
 pub const YOLO_MODEL_URL: &str =
-    "https://github.com/neutrinographics/faceguard/releases/download/v0.1.0/yolo11n-pose_widerface.onnx";
+    "https://github.com/neutrinographics/faceguard-ml-models/releases/download/models-v1/yolo11n-pose_widerface.onnx";
 
 pub const EMBEDDING_MODEL_NAME: &str = "w600k_r50.onnx";
 pub const EMBEDDING_MODEL_URL: &str =
-    "https://github.com/neutrinographics/faceguard/releases/download/v0.1.0/w600k_r50.onnx";
+    "https://github.com/neutrinographics/faceguard-ml-models/releases/download/models-v1/w600k_r50.onnx";
 
 /// Max frames a track can be lost before removal (~1 second at 30 fps).
 pub const TRACKER_MAX_LOST: usize = 30;
